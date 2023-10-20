57db988371cbba0edcc40a7cad469080f7b2df44
Tashingaaa <tashii7.mlilwana@gmail.com>
2023-10-21 00:17:50 +0200

    Updated the time

--- src/app.js
+++ src/app.js
@@ -1,17 +1,39 @@
+function formatDate (timestamp) {
+let date = new Date(timestamp);
+let hours = date.getHours();
+
+if (hours < 10) {
+  hours = `0${hours}`;
+}
+let minutes = date.getMinutes();
+if (minutes < 10) {
+    minutes = `0${minutes}`;
+}
+let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+let day = days[date.getDay()];
+return `${day}, ${hours}:${minutes}`;
+}
+
+
+
+
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#weather-description");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
+  let datetimeElement = document.querySelector("#date-time");
   temperatureElement.innerHTML = Math.round(response.data.temperature.current);
   cityElement.innerHTML = response.data.city;
   descriptionElement.innerHTML = response.data.condition.description;
   humidityElement.innerHTML = response.data.temperature.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
+  datetimeElement.innerHTML = formatDate(response.data.time*1000);
 }
 
 let apiKey = "141397dcf9b1a404fb4094a07d7t93ao";
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query=London&key=${apiKey}&units=metric`;
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=Johannesburg&key=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
