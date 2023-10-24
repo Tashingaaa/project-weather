137dfdffa726a3dc3c8fa089ad1d3ee1d9ad63c7
Tashingaaa <tashii7.mlilwana@gmail.com>
2023-10-24 15:02:28 +0200

    Implemented the unit conversions

--- src/app.js
+++ src/app.js
@@ -1,22 +1,26 @@
-function formatDate (timestamp) {
-let date = new Date(timestamp);
-let hours = date.getHours();
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
 
-if (hours < 10) {
-  hours = `0${hours}`;
-}
-let minutes = date.getMinutes();
-if (minutes < 10) {
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
     minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day}, ${hours}:${minutes}`;
 }
-let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
-let day = days[date.getDay()];
-return `${day}, ${hours}:${minutes}`;
-}
-
-
-
-
 
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
@@ -27,36 +31,59 @@ function displayTemperature(response) {
   let datetimeElement = document.querySelector("#date-time");
   let iconElement = document.querySelector("#icon");
 
+  celsiusTemperature = Math.round(response.data.temperature.current);
 
   temperatureElement.innerHTML = Math.round(response.data.temperature.current);
   cityElement.innerHTML = response.data.city;
   descriptionElement.innerHTML = response.data.condition.description;
   humidityElement.innerHTML = response.data.temperature.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
-  datetimeElement.innerHTML = formatDate(response.data.time*1000);
+  datetimeElement.innerHTML = formatDate(response.data.time * 1000);
   iconElement.setAttribute(
     "src",
-     `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`
-     ); 
-     iconElement.setAttribute("alt", response.data.condition.description);
+    `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`
+  );
+  iconElement.setAttribute("alt", response.data.condition.description);
 }
 
-function search (city){
-
-    let apiKey = "141397dcf9b1a404fb4094a07d7t93ao";
-    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-    axios.get(apiUrl).then(displayTemperature);
+function search(city) {
+  let apiKey = "141397dcf9b1a404fb4094a07d7t93ao";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
 }
 
-
-function handleSubmit (event) {
-    event.preventDefault();
-    let cityInputElement = document.querySelector("#city-input");
-    search(cityInputElement.value);
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
 }
 
-search("New York");
+let celsiusTemperature = null;
+
+function convertToFahrenheit(event) {
+  event.preventDefault();
+  let fahrenheitTemp = (celsiusTemperature * 9) / 5 + 32;
+  celsiusLink.classList.remove("active");
+  fahrenheitLink.classList.add("active");
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(fahrenheitTemp);
+}
+
+function convertToCelsius(event) {
+  event.preventDefault();
+  let celsiusTemp = document.querySelector("#temperature");
+  celsiusLink.classList.add("active");
+  fahrenheitLink.classList.remove("active");
+  celsiusTemp.innerHTML = celsiusTemperature;
+}
 
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
 
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", convertToFahrenheit);
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", convertToCelsius);
+
+search("Johannesburg");
